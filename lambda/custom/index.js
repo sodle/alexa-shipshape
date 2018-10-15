ecf3794c2d60f7edbcaa1a766f48f087873733e1
Scott Odle <scott@sjodle.com>
2018-10-15 06:02:01 +0000

    launchintent

---
+++ lambda/custom/index.js
@@ -0,0 +1,116 @@
+/* eslint-disable  func-names */
+/* eslint-disable  no-console */
+
+const Alexa = require('ask-sdk-core');
+import { Player } from './shipshape';
+
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+  },
+  async handle(handlerInput) {
+    let [player, playerCreated] = await Player.findOrCreate({
+        where: {
+            alexaUid: handlerInput.requestEnvelope.session.user.userId
+        }
+    }).spread((player, playerCreated) => [player, playerCreated]);
+
+    if (playerCreated) {
+      return handlerInput.responseBuilder
+        .speak(`Welcome to ShipShape! I see it's your first time playing. You can say "start a new game" to begin.`)
+        .reprompt(`Say "start a new game" to begin.`)
+        .getResponse();
+    }
+
+    let match = await player.getActiveMatch();
+
+    if (match === null) {
+      return handlerInput.responseBuilder
+        .speak(`Welcome to ShipShape! Say "start" to play.`)
+        .reprompt(`Say "start" to play.`)
+        .getResponse();
+    }
+
+    let unplaced = await match.getUnplacedShips(false);
+    if (unplaced.length !== 0) {
+      return handlerInput.responseBuilder
+        .speak(`Welcome to ShipShape! You have unplaced ships. Where would you like to place your ${unplaced[0].name}? It's ${unplaced[0].length} squares long.`)
+        .reprompt(`Where would you like to place your ${unplaced[0].name}? It's ${unplaced[0].length} squares long.`)
+        .getResponse();
+    }
+
+    return handlerInput.responseBuilder
+      .speak(`Welcome to ShipShape! It's your move!`)
+      .reprompt(`It's your move!`)
+      .getResponse();
+  },
+};
+
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'You can say hello to me!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .withSimpleCard('Hello World', speechText)
+      .getResponse();
+  },
+};
+
+const CancelAndStopIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    const speechText = 'Goodbye!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard('Hello World', speechText)
+      .getResponse();
+  },
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak('Sorry, I can\'t understand the command. Please say again.')
+      .reprompt('Sorry, I can\'t understand the command. Please say again.')
+      .getResponse();
+  },
+};
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    HelpIntentHandler,
+    CancelAndStopIntentHandler,
+    SessionEndedRequestHandler
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
---
+++ lambda/custom/shipshape.js
@@ -0,0 +1,358 @@
+import Sequelize, { Op, DataTypes } from 'sequelize';
+
+const sequelize = new Sequelize('shipshape', process.env.SEQUELIZE_USER, process.env.SEQUELIZE_PASS, {
+    dialect: 'postgres',
+    host: process.env.SEQUELIZE_HOST,
+    operatorsAliases: Op,
+    logging: false
+});
+
+const getSquareIdx = (x, y) => y * 10 + x;
+
+const Player = sequelize.define('player', {
+   alexaUid: {
+       type: Sequelize.STRING,
+       unique: true
+   }
+});
+Player.prototype.getAllMatches = function () {
+    return Match.findAll({
+        include: [
+            {
+                model: Player,
+                where: {
+                    id: {
+                        [Op.eq]: Sequelize.col('player.id')
+                    }
+                }
+            }
+        ]
+    });
+};
+Player.prototype.getActiveMatch = function () {
+    return Match.findOne({
+        include: [
+            {
+                model: Player,
+                where: {
+                    id: {
+                        [Op.eq]: Sequelize.col('player.id')
+                    }
+                }
+            }
+        ],
+        where: {
+            endTime: {
+                [Op.eq]: null
+            }
+        }
+    });
+};
+Player.prototype.getOrStartMatch = async function (opponentIfNew) {
+    let match = await this.getActiveMatch();
+    let created = match === null;
+    if (created)
+        match = await this.startMatch(opponentIfNew);
+    return [match, created];
+}
+Player.prototype.getMatchHistory = function () {
+    return Match.findAll({
+        include: [
+            {
+                model: Player,
+                where: {
+                    id: {
+                        [Op.eq]: Sequelize.col('player.id')
+                    }
+                }
+            }
+        ],
+        where: {
+            endTime: {
+                [Op.not]: null
+            }
+        }
+    });
+};
+Player.prototype.startMatch = function (opponent) {
+    return new Promise((resolve, reject) => {
+        this.getActiveMatch().then((existingMatch) => {
+            if (existingMatch === null) {
+                Match.create({
+                    aiOpponentType: opponent,
+                    humanBoard: '.'.repeat(100),
+                    aiBoard: '.'.repeat(100),
+                    startTime: Date.now(),
+                    playerId: this.id
+                }).then((newMatch) => resolve(newMatch));
+            } else
+                reject(Error(`Match already in progress.`));
+        });
+    });
+};
+
+const Match = sequelize.define('match', {
+    aiOpponentType: {
+        type: Sequelize.STRING
+    },
+    humanBoard: {
+        type: Sequelize.STRING
+    },
+    aiBoard: {
+        type: Sequelize.STRING
+    },
+    aiWin: {
+        type: Sequelize.BOOLEAN
+    },
+    startTime: {
+        type: Sequelize.DATE
+    },
+    endTime: {
+        type: Sequelize.DATE
+    }
+});
+Match.belongsTo(Player);
+Match.prototype.getPlayerShips = function(ai) {
+    return Ship.findAll({
+        include: [
+            {
+                model: Match,
+                where: {
+                    id: {
+                        [Op.eq]: Sequelize.col('match.id')
+                    }
+                }
+            }
+        ],
+        where: {
+            ai: {
+                [Op.eq]: ai
+            }
+        }
+    });
+};
+Match.prototype.listAiMoves = function() {
+    return Move.findAll({
+        include: [
+            {
+                model: Match,
+                where: {
+                    id: {
+                        [Op.eq]: Sequelize.col('match.id')
+                    }
+                }
+            }
+        ],
+        where: {
+            ai: {
+                [Op.eq]: true
+            }
+        },
+        order: [
+            ['date', 'DESC']
+        ]
+    });
+};
+Match.prototype.getUnplacedShips = function(ai) {
+    let ships = {
+        Carrier: 5,
+        Battleship: 4,
+        Submarine: 3,
+        Cruiser: 3,
+        Destroyer: 2
+    };
+    return new Promise((resolve, reject) => {
+        this.getPlayerShips(ai).then((playerShips) => {
+            for (let ship of playerShips) {
+                if (ships.hasOwnProperty(ship.name) && ship.length === ships[ship.name])
+                    delete ships[ship.name];
+                else
+                    reject(`Unknown ship ${ship.name} (${ship.length})`);
+            }
+            resolve(ships);
+        });
+    });
+};
+Match.prototype.checkShipUnplaced = function(name, length, ai) {
+    return new Promise((resolve, reject) => {
+        this.getUnplacedShips(ai).then((unplaced) => {
+            if (unplaced.hasOwnProperty(name))
+                if (unplaced[name] === length)
+                    resolve();
+                else
+                    reject(`Expected ${name} to be length ${unplaced[name]}, not ${length}`);
+            else
+                reject(`Ship ${name} invalid or already placed.`);
+        });
+    });
+};
+Match.prototype.checkShipOverlap = async function(newShip) {
+    let ships = await this.getPlayerShips(newShip.ai);
+    for (let ship of ships) {
+        for (let square of ship.getSquares()) {
+            let [x, y] = square;
+            for (let newSquare of newShip.getSquares()) {
+                let [newX, newY] = newSquare;
+                if (x === newX && y === newY) {
+                    throw new Error(`Overlaps with ${ship.name} at ${'ABCDEFGHIJ'[x]}${y + 1}!`);
+                }
+            }
+        }
+    }
+};
+Match.prototype.placeShip = async function(name, length, x, y, vertical, ai) {
+    await this.checkShipUnplaced(name, length, ai);
+    if (x < 0 || y < 0 || x >= 10 || y >= 10) {
+        throw new Error('Ship off board');
+    }
+    if (vertical) {
+        if ((y + length) > 10) {
+            throw new Error('Ship off board');
+        }
+    } else {
+        if ((x + length) > 10) {
+            throw new Error('Ship off board');
+        }
+    }
+    let newShip = Ship.build({
+        name: name,
+        length: length,
+        x: x,
+        y: y,
+        vertical: vertical,
+        ai: ai,
+        matchId: this.id
+    });
+    await this.checkShipOverlap(newShip);
+    return newShip.save();
+};
+Match.prototype.makeMove = async function(x, y, ai) {
+    let boardToCheck = (ai) ? this.humanBoard : this.aiBoard;
+    let squareToCheck = getSquareIdx(x, y);
+    if (boardToCheck[squareToCheck] === '.') {
+        let hit = null;
+        let moveLog = Move.build({
+            x,
+            y,
+            ai,
+            matchId: this.id
+        });
+        for (let ship of await this.getPlayerShips(!ai)) {
+            if (await ship.checkHit(x, y)) {
+                hit = ship;
+                moveLog.shipId = ship.id;
+            }
+        }
+        boardToCheck = boardToCheck.split('');
+        boardToCheck[squareToCheck] = (hit !== null) ? 'x' : 'o';
+        boardToCheck = boardToCheck.join('');
+        if (ai)
+            this.humanBoard = boardToCheck;
+        else
+            this.aiBoard = boardToCheck;
+        await moveLog.save();
+        await this.save();
+        return hit;
+    } else {
+        throw new Error('Already a move here');
+    }
+};
+Match.prototype.checkWin = function(ai) {
+    return new Promise((resolve, reject) => {
+        this.getPlayerShips(!ai).then((ships) => {
+            for (let ship of ships) {
+                if (!ship.checkSunk()) {
+                    resolve(false);
+                    return;
+                }
+            }
+            this.endTime = new Date();
+            this.aiWin = ai;
+            this.save().then(() => resolve(true)).catch((error) => reject(error));
+        }).catch((error) => reject(error));
+    });
+};
+
+const Ship = sequelize.define('ship', {
+    name: {
+        type: Sequelize.STRING
+    },
+    x: {
+        type: Sequelize.INTEGER
+    },
+    y: {
+        type: Sequelize.INTEGER
+    },
+    length: {
+        type: Sequelize.INTEGER
+    },
+    vertical: {
+        type: Sequelize.BOOLEAN
+    },
+    ai: {
+        type: Sequelize.BOOLEAN
+    },
+    hits: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0
+    }
+});
+Ship.belongsTo(Match);
+Ship.prototype.getSquares = function() {
+    let squares = [];
+    for (let i = 0; i < this.length; i++)
+        if (this.vertical)
+            squares.push([this.x, this.y + i]);
+        else
+            squares.push([this.x + i, this.y]);
+    return squares;
+};
+Ship.prototype.checkHit = async function(x, y) {
+    for (let [sX, sY] of this.getSquares()) {
+        if (sX === x && sY === y) {
+            await this.increment('hits', {by: 1});
+            await this.save();
+            return true;
+        }
+    }
+    return false;
+};
+Ship.prototype.checkSunk = function() {
+    return this.length === this.hits;
+};
+
+const Move = sequelize.define('move', {
+    x: {
+        type: Sequelize.INTEGER
+    },
+    y: {
+        type: Sequelize.INTEGER
+    },
+    date: {
+        type: Sequelize.DATE,
+        defaultValue: DataTypes.NOW
+    },
+    ai: {
+        type: Sequelize.BOOLEAN
+    }
+});
+Move.belongsTo(Match);
+Move.belongsTo(Ship);
+
+const syncDb = () => new Promise((resolve) => {
+    Player.sync().then(() => {
+        console.log('Player model create success');
+        Match.sync().then(() => {
+            console.log('Match object create success');
+            Ship.sync().then(() => {
+                console.log('Ship model create success');
+                Move.sync().then(() => {
+                    console.log('Move model create success');
+                    resolve();
+                });
+            });
+        });
+    });
+});
+
+export { Player, Match, Ship, syncDb, getSquareIdx };
