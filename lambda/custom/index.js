95bf421c7f202efcd2cb941b0b176a2d7ca7efd4
Scott Odle <sjo@asu.edu>
2018-10-15 13:52:50 -0700

    start game intent

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -2,16 +2,13 @@
 /* eslint-disable  no-console */
 
 const Alexa = require('ask-sdk');
-const { Player, syncDb } = require('./shipshape');
+const { Player } = require('./shipshape');
 
 const LaunchRequestHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
   },
   async handle(handlerInput) {
-    console.log('Sync DB');
-    await syncDb();
-
     console.log('Launch');
     let [player, playerCreated] = await Player.findOrCreate({
         where: {
@@ -41,7 +38,7 @@ const LaunchRequestHandler = {
     }
 
     let unplaced = await match.getUnplacedShips(false);
-    unplaced = Object.keys(unplaced).map(key => [key, unplaced[key]]);
+    unplaced = Object.keys(unplaced).map(key => {return {name: key, length: unplaced[key]}});
     if (unplaced.length !== 0) {
       return handlerInput.responseBuilder
         .speak(`Welcome to ShipShape! You have unplaced ships. Where would you like to place your ${unplaced[0].name}? It's ${unplaced[0].length} squares long.`)
@@ -56,13 +53,64 @@ const LaunchRequestHandler = {
   }
 };
 
+const StartIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'StartIntent';
+  },
+  async handle(handlerInput) {
+    let [player, playerCreated] = await Player.findOrCreate({
+      where: {
+          alexaUid: handlerInput.requestEnvelope.session.user.userId
+      }
+    }).spread((player, playerCreated) => [player, playerCreated]);
+
+    if (await player.getActiveMatch() !== null) {
+      return handlerInput.responseBuilder
+        .speak('You already have a game in progress. Make your move, or say "forfeit".')
+        .reprompt('What would you like to do?')
+        .getResponse();
+    }
+
+    let match = await player.startMatch('bender');
+
+    let unplacedAi = await match.getUnplacedShips(true);
+    unplacedAi = Object.keys(unplacedAi).map(key => [key, unplacedAi[key]]);
+    if (unplacedAi.length !== 0) {
+        console.log('Placing AI ships.');
+        for (let unplacedShip of unplacedAi) {
+            let [name, len] = unplacedShip;
+            let success = false;
+            console.log(`Placing ${name} - ${len}`);
+            while (!success) {
+                let [vertical, x, y] = placeShipRandomly(len);
+                try {
+                    await match.placeShip(name, len, x, y, vertical, true);
+                    console.log(`Placed ${name}`);
+                    success = true;
+                } catch(e) {
+                    console.log(e);
+                }
+            }
+        }
+    }
+
+    let unplacedFriendly = await match.getUnplacedShips(false);
+    unplacedFriendly = Object.keys(unplacedFriendly).map(key => {return {name: key, length: unplacedFriendly[key]}});
+    return handlerInput.responseBuilder
+      .speak(`It's time to place your ships. Where would you like to place your ${unplacedFriendly[0].name}? It's ${unplacedFriendly[0].length} squares long.`)
+      .reprompt(`Where would you like to place your ${unplacedFriendly[0].name}? It's ${unplacedFriendly[0].length} squares long.`)
+      .getResponse();
+  }
+}
+
 const HelpIntentHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
       && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
   },
   handle(handlerInput) {
-    const speechText = 'You can say hello to me!';
+    const speechText = 'Say "start" to start a new game.';
 
     return handlerInput.responseBuilder
       .speak(speechText)
@@ -118,6 +166,7 @@ const skillBuilder = Alexa.SkillBuilders.standard();
 exports.handler = skillBuilder
   .addRequestHandlers(
     LaunchRequestHandler,
+    StartIntentHandler,
     HelpIntentHandler,
     CancelAndStopIntentHandler,
     SessionEndedRequestHandler
